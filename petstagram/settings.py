b527b6160ccf5755b15ac22f8def403854631740
Bistrev Lyubomir <lubo_bistrev@abv.bg>
2022-10-07 18:40:08 +0300

    Skip auth_pasword in settins.py for test

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -103,18 +103,18 @@ AUTH_PASSWORD_VALIDATORS = []
 
 if is_production():
     AUTH_PASSWORD_VALIDATORS.extend([
-        {
-            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-        },
+        # {
+        #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        # },
+        # {
+        #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        # },
+        # {
+        #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        # },
+        # {
+        #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        # },
 
     ])
 
