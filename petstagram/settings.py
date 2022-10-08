2cbf3f365c75f3d1928c67e1a6154e238af20597
Bistrev Lyubomir <lubo_bistrev@abv.bg>
2022-10-08 09:24:40 +0300

    Return DATABASE confing in settings.py

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -83,14 +83,23 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 APP_ENVIRONMENT = os.getenv('APP_ENVIRONMENT', 'Development')
 
 DEFAULT_DATABASE_CONFIG = {
-    'ENGINE': 'django.db.backends.postgresql',
-    'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-    'PORT': os.getenv('DB_PORT', '5432'),  # if no .env variable DB_PORT, return '5432'
-    'NAME': os.getenv('DB_NAME', 'petstagram_db'),
-    'USER': os.getenv('DB_USER', 'postgres'),
-    'PASSWORD': os.getenv('DB_PASSWORD', '0123AsD'),
-
-}
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'petstagram_db',
+        'USER': 'postgres',
+        'PASSWORD': '0123AsD',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+    
+if is_production():
+    DEFAULT_DATABASE_CONFIG = {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),  # if no .env variable DB_PORT, return '5432'
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+    }
 
 DATABASES = {
     'default': DEFAULT_DATABASE_CONFIG,
